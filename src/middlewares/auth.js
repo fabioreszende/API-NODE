7c7497195a8bbdd6a80dde4efd06b2bcc0a2bd89
fabioreszende <fabiorezendefilho@gmail.com>
2020-10-31 21:31:10 -0300

    up

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -16,4 +16,11 @@ module.exports = (req, res, next) => {
 
   if (!/^Bearer$/i.test(scheme))
     return res.status(401).send({ error: 'Token malformatted' });
+
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) return res.status(401).send({ error: 'Token invalid' });
+
+        req.userId = decoded.id;
+        return next();
+    });
 };
